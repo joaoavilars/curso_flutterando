cf0cb25c458e29a0b68ff4d1819870284fbb5292
joaoavilars <joao.avila.rs@gmail.com>
2023-12-18 20:27:08 -0300

    primeiro commit

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('João estanciado'),
+    );
+  }
+}
